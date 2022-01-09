330642ffb66d2015cd833208cd03b7fd1e2c2eb4
zhuyuchen <zhuyuchen@kuaishou.com>
2022-01-09 22:23:02 +0800

    feat: add simple auth system via auth0

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,26 @@
-/*
- * @file: file description
- * @author: your name
- * @Date: 2021-10-21 17:14:56
- * @LastEditors: your name
- * @LastEditTime: 2022-01-09 20:28:31
- */
-
-import React from "react";
-import { useState, useEffect } from "react";
-import { Switch, Route, Redirect } from "react-router-dom";
-import { loadScript, loadClient, authenticate } from "./server/gapiService";
+// import React from "react";
+// import { useState, useEffect } from "react";
+// import { Switch, Route, Redirect } from "react-router-dom";
+// import { loadScript, loadClient, authenticate } from "./server/gapiService";
 
 // import "./App.css";
 // import { fetchPlaylists } from "./service/youtubeApiService";
 // import PlaylistDisplay from "./components/PlaylistDisplay";
 // import Loading from "./components/Loading";
-import LoginPage from "./client/features/page/login-page/LoginPage";
-import HomePage from "./client/features/page/home-page/HomePage";
-import Loading from "./client/features/Loading";
+// import LoginPage from "./client/features/page/login-page/LoginPage";
+// import HomePage from "./client/features/page/home-page/HomePage";
+// import Loading from "./client/features/Loading";
 import RouterContainer from "./client/features/container/RouterContainer";
 
 const App = () => {
   // const [isGapiLoaded, setGapiLoaded] = useState(false);
-  const [isLogin, setLogin] = useState(false);
+  // const [isLogin, setLogin] = useState(false);
   // const [isClientLoaded, setClientLoaded] = useState(false);
 
 
-  useEffect(() => {
-    // setLogin(getLoginStatus());
-  }, [])
+  // useEffect(() => {
+  //   setLogin(getLoginStatus());
+  // }, [])
 
   // useEffect(() => {
   //   document.body.appendChild(loadScript(setGapiLoaded));
